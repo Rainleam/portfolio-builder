1173c98daae569e0ce2a84e2f57001097cb81add
Marialena <marialena.alevizaki@gmail.com>
2024-01-21 17:21:12 +0200

    Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, FormsModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'portfolio-builder';
+
+  nm = '';
+  em = '';
+  ph = '';
+  add = '';
+  submitted: boolean = false;
+  qualifications = [{school: '', degree: '', year: ''}];
+
+  addQualification() {
+    this.qualifications.push({school: '', degree: '', year: ''});
+  }
+
+  submit() {
+    this.submitted = true;
+  }
+
+  edit() {
+    this.submitted = false;
+  }
 }
